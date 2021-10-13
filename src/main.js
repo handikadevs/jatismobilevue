2fff16abde24f9580747e152ce7bb3238e744e71
HandjoeIT <86072354+HandjoeIT@users.noreply.github.com>
2021-10-13 16:25:05 +0700

    update-1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Argon from "./plugins/argon-kit";
-import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 Vue.use(Argon);
